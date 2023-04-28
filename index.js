0fae138cce69b3b0b6baae1a04f2b25e652ff5ca
JoseEducordova <josejojocarvalho@gmail.com>
2023-04-27 21:37:10 -0300

    aula dia 27/04

---
+++ index.js
@@ -0,0 +1,20 @@
+/*const nome = prompt("nome?")
+console.log (`O ${nome} "é turista"`);*/
+
+/*const nome = prompt ("Qual seu nome? ")
+const corFavorita = prompt("Qual sua cor favorita? ")
+
+console.log(`Seu nome é ${nome}  e sua cor favorita é  ${corFavorita}`);
+console.log("Seu nome é: ", nome , "e sua cor favorita é: " , corFavorita );*/
+
+/*const nome = prompt ("digite seu nome: ")
+console.log(nome.toLowerCase())
+console.log(nome.toUpperCase())*/
+
+/*const frase= prompt ("Digite uma frase: ");
+
+console.log(frase.toUpperCase());
+console.log(frase.replaceAll("o", "i"));
+console.log(frase.length);
+
+console.log(frase.replaceAll("o", "i").toUpperCase(), frase.length);*/
